426f90ffb28d11c7c409a3df41d332c848185ec6
Jeffrey <jeffreychai118@gmail.com>
2018-04-05 17:24:38 +0800

    Update env port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const apiai = require('apiai')('8225da3f7e6f4302bcc514722d4ff336');
 const app = require('express')();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -12,24 +13,7 @@ var request = apiai.textRequest('hello there aibou', {
 // for demo purposes
 app.get('/notes', (req,res) => {
   res.json({notes: 'This is your notebook.'})
-  res.end();
 });
-
-// app.get('/api/idk', (req, res) => {
-//   request.on('response', (response) => {
-//     // console.log(response);
-//     return res.json(response);
-//   });
-  
-//   request.on('error', (error) => {
-//     // console.log(error);
-//     return res.json(error);
-//   });
-  
-//   request.end();
-//   req.on('error', err=>console.error(err));
-//   res.end();
-// });
 
 // user POST converstaion with bot
 app.post('/api/text', (req, res) => {
@@ -50,33 +34,6 @@ app.post('/api/text', (req, res) => {
   res.end();
 });
 
-app.listen(3000, () => {
-  console.log('Server up');
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
-
-// response.result.fulfillment.speech
-
-// const getRes = (query) => {
-//   const request = app.textRequest(query, {
-//     sessionId: '12345'
-//   });
-
-//   const responseFromAPI = new Promise((resolve, reject) => {
-//     request.on('error', (error) => {
-//       reject(error);
-//     });
-
-//     request.on('response', (response) => {
-//       resolve(response.result.fulfillment.speech);
-//     });
-//   });
-//   request.end();
-
-//   return responseFromAPI;
-// };
-
-// getRes('hello').then((res) => {
-//   console.log(res);
-// });
-
-// module.exports = {getRes};
