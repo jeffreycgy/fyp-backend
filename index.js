60ded11299b8804a88ea7b4d8e98ed805180b630
Jeffrey <jeffreychai118@gmail.com>
2018-04-08 18:14:02 +0800

    Fix socket hang up

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+if(process.env.NODE_ENV !== 'production') {
+  require('longjohn');
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-var request = apiai.textRequest('hello there aibou', {
-  sessionId: '12345'
-});
+// let request = apiai.textRequest('hello there aibou', {
+//   sessionId: '12345'
+// });
 
 // for demo purposes
 app.get('/notes', (req,res) => {
   res.json({notes: 'This is your notebook.'})
+  req.on('error', err=>console.error(err));
 });
 
 // user POST converstaion with bot
 app.post('/api/text', (req, res) => {
-  console.log('called');
-  request = apiai.textRequest(req.body.query, {
+  let request = apiai.textRequest(req.body.query, {
     sessionId: '12345'
   });
   request.on('response', (response) => {
     return res.json(response.result.fulfillment.speech);
   });
-  
   request.on('error', (error) => {
     return res.json(error.result.fulfillment.speech);
   });
